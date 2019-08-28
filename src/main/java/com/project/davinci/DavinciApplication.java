692d2fa7a4448ea9eae2f2968aebcdd904e79964
lenovo <lajcsu@vip.qq.com>
2019-08-28 20:49:34 +0800

    original version

---
+++ src/main/java/com/project/davinci/DavinciApplication.java
@@ -0,0 +1,13 @@
+package com.project.davinci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavinciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DavinciApplication.class, args);
+    }
+
+}
