5a195cab156e80fa1c3d6c84da402fd240077ca9
97534 <975344363@qq.com>
2019-08-29 14:15:01 +0800

    建目录

---
+++ src/main/resources/static/js/index_need/common.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?790d31476c7360e6af7afd64331e2d06";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
