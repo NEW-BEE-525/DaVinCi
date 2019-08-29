d9e9b53f8b07c466433a4bccf23b6dbe1f11669b
ASUS1 <1329004166@qq.com>
2019-08-29 09:25:11 +0800

    test

--- src/main/java/com/project/davinci/DavinciApplication.java
+++ src/main/java/com/project/davinci/DavinciApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DavinciApplication {
 
+    //123
     public static void main(String[] args) {
         SpringApplication.run(DavinciApplication.class, args);
     }
